b40dffebb91702fe38755319eb3b033e12525fa3
Kristina Adigamova <kriwowak@yandex.ru>
2023-03-29 20:44:41 +0500

    Initial commit

---
+++ src/main/java/com/example/testirovanie/TestirovanieApplication.java
@@ -0,0 +1,13 @@
+package com.example.testirovanie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestirovanieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestirovanieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testirovanie/TestirovanieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testirovanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestirovanieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
